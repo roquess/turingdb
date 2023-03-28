1ed8209cb98bd4470f26ff4a24467918235c84a4
remy <remy.boutonnet@turing.bio>
2023-03-28 20:53:48 +0000

    Setting TURING_HOME in turing.setup.sh

--- turing.setup.sh
+++ turing.setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [[ -z "$TURING_HOME" ]] ; then
-    echo "ERROR: please set TURING_HOME."
-    exit 1
-fi
+# Install directory
+export TURING_HOME=/home/dev/turing/turing_install
 
 export PATH=$TURING_HOME/bin:$PATH
 export PYTHONPATH=$TURING_HOME/lib/python:$PYTHONPATH
