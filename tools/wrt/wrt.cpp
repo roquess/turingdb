09f93de9a2530a77b643003f9b7d58a7414fe7cf
Remy Boutonnet <remy.boutonnet@turing.bio>
2023-11-20 11:15:09 +0000

    wrt: remove timeout and add concurrency -j option

--- tools/wrt/wrt.cpp
+++ tools/wrt/wrt.cpp
@@ -37,24 +37,17 @@ int main(int argc, const char** argv) {
     ArgParser& argParser = toolInit.getArgParser();
     argParser.addOption("clean", "Clean all test directories");
     argParser.addOption("noclean", "Do not clean test directories");
-    argParser.addOption("j", "Number of parallel jobs", "jobs");
 
     toolInit.init(argc, argv);
 
     bool cleanDir = false;
     bool cleanIfSuccess = true;
     bool error = false;
-    size_t jobs = 1;
     for (const auto& option : argParser.options()) {
         if (option.first == "clean") {
             cleanDir = true;
         } else if (option.first == "noclean") {
             cleanIfSuccess = false;
-        } else if (option.first == "j") {
-            jobs = StringToNumber<size_t>(option.second, error);
-            if (error || jobs == 0) {
-                BioLog::log(msg::ERROR_WRT_BAD_NUMBER_OF_JOBS() << option.second);
-            }
         }
     }
 
@@ -69,7 +62,6 @@ int main(int argc, const char** argv) {
     signal(SIGTERM, signalHandler);
 
     regress->setCleanIfSuccess(cleanIfSuccess);
-    regress->setConcurrency(jobs);
 
     if (cleanDir) {
         regress->clean();
