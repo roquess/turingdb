3cc5c1b501439c3cdb22d4448dac60377a79f5cf
Remy Boutonnet <remy.boutonnet@turing.bio>
2024-02-09 15:10:32 +0000

    Implement skeleton of pipelines

---
+++ test/db2/pipeline/PipelineTest.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+#include "PipelineManager.h"
+
+using namespace db;
+
+class PipelineTest : public ::testing::Test {
+};
+
+TEST_F(PipelineTest, test1) {
+    PipelineManager pipeMan;
+}
