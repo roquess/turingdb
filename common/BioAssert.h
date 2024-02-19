8583465657ea81d243f2c1f1f4363fd2da5be5e4
Remy Boutonnet <remy.boutonnet@turing.bio>
2024-02-19 20:48:34 +0000

    DB: implementation of query planner with pipeline construction

--- common/BioAssert.h
+++ common/BioAssert.h
@@ -1,9 +1,6 @@
-#ifndef _BIO_ASSERT_
-#define _BIO_ASSERT_
+#pragma once
 
 void __bioAssertWithLocation(const char* file, unsigned line, const char* expr, const char* msg = "");
 
 #define bioassert(C) ({ if (!(C)) { __bioAssertWithLocation(__FILE__, __LINE__, #C); }})
 #define msgbioassert(C, msg) ({ if (!(C)) { __bioAssertWithLocation(__FILE__, __LINE__, #C, msg); }})
-
-#endif
