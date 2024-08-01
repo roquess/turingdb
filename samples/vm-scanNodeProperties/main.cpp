101b0090049ef00347ba3a102eaa371e04133166
luclabarriere <luc.labarriere@turing.bio>
2024-08-01 12:08:46 +0000

    316: Property iteration implementation in the VM

---
+++ samples/vm-scanNodeProperties/main.cpp
@@ -0,0 +1,27 @@
+#include "vmutils.h"
+
+using namespace db;
+
+int main() {
+    auto sample = VMSample::createSample(SAMPLE_NAME);
+
+    sample.createSimpleGraph();
+    //if (!sample.loadJsonDB(sample._turingHome + "/neo4j/pole-db/")) {
+    //    return 1;
+    //}
+
+    if (!sample.generateFromFile(sample._sampleDir + "/program.turing")) {
+        return 1;
+    }
+
+    sample.execute();
+    sample.printOutput({
+        "SrcID",
+        "EdgeID",
+        "EdgeType",
+        "TgtID",
+    });
+    sample.destroy();
+
+    return 0;
+}
