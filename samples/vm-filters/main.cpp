593af216ded4f86a94e61a5e5f5f8d9da990d196
luclabarriere <luc.labarriere@turing.bio>
2024-08-14 13:53:16 +0000

    317: filters in the vm

--- samples/vm-filters/main.cpp
+++ samples/vm-filters/main.cpp
@@ -5,31 +5,22 @@ using namespace db;
 int main() {
     auto sample = VMSample::createSample(SAMPLE_NAME);
 
-    sample.createSimpleGraph();
-    //if (!sample.loadJsonDB(sample._turingHome + "/neo4j/pole-db/")) {
-    //if (!sample.loadJsonDB("/home/dev/json-dbs/reactome-json")) {
-    //    return 1;
-    //}
+    if (!sample.loadJsonDB(sample._turingHome + "/neo4j/pole-db/")) {
+        return 1;
+    }
 
     if (!sample.generateFromFile(sample._sampleDir + "/program.turing")) {
         return 1;
     }
 
     sample.execute();
-    sample.printOutputProperty("name", {
+    sample.printOutput({
         "  (n1)  ",
-        //"  [EdgeID  :",
-        //"EdgeType]",
         "->(n2)  ",
-        //"  [EdgeID  :",
-        //"EdgeType]",
         "->(n3)  ",
     }, 0, 20);
-    sample.destroy();
 
-    //for (const auto& p : sample.readDB().scanNodeProperties<types::String>(0)) {
-    //    spdlog::info(p);
-    //}
+    sample.destroy();
 
     return 0;
 }
