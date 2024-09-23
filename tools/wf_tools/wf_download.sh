12f63a3182d8b9d1db3d0d93d616cbe2069a6d62
martina <martina.rimoldi@turing.bio>
2024-09-23 12:21:49 +0000

    wf download: update of requirement.json parsing and .get.txt data fetching

--- tools/wf_tools/wf_download.sh
+++ tools/wf_tools/wf_download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eou pipefail
 
 PROJECT=$1
 DATASET=$2
@@ -9,12 +10,19 @@ OTP=$4
 ast sync ${S3}/${PROJECT}/${DATASET} ${OTP}/${PROJECT}/${DATASET}
 echo -e "\n"
 
-## TO ADD: -> check if it is a folder or a file in s3bucket and use either sync or cp. it could also be a local file, not in the bucket
 if [[ -f ${OTP}/${PROJECT}/${DATASET}/data/.getdata.txt ]]; then 
     echo -e "Fetching data from:"
     for path in `cat ${OTP}/${PROJECT}/${DATASET}/data/.getdata.txt`; do
         echo -e "$path"
-        ast sync ${path} ${OTP}/${PROJECT}/${DATASET}/data/01.Data
+        if [[ $( ast typecheck ${path} | awk '{print $3}' ) == "file" ]]; then
+            fl_name=$(basename ${path})
+            ast syncup ${path} ${OTP}/${PROJECT}/${DATASET}/data/01.Data/${fl_name}
+        elif [[ $( ast typecheck ${path} | awk '{print $3}' ) == "folder" ]]; then
+            ast syncup ${path} ${OTP}/${PROJECT}/${DATASET}/data/01.Data/
+        else
+            echo -e "[S3Uri error]: check provided path in .getdata.txt file."
+            exit 1
+        fi
     done
 fi
 
@@ -22,7 +30,15 @@ if [[ -f ${OTP}/${PROJECT}/${DATASET}/metadata/.getmetadata.txt ]]; then
     echo -e "Fetching data from:"
     for path in `cat ${OTP}/${PROJECT}/${DATASET}/metadata/.getmetadata.txt`; do
         echo -e "$path"
-        ast sync ${path} ${OTP}/${PROJECT}/${DATASET}/metadata/
+        if [[ $( ast typecheck ${path} | awk '{print $3}' ) == "file" ]]; then
+            fl_name=$(basename ${path})
+            ast syncup ${path} ${OTP}/${PROJECT}/${DATASET}/data/01.Data/${fl_name}
+        elif [[ $( ast typecheck ${path} | awk '{print $3}' ) == "folder" ]]; then
+            ast syncup ${path} ${OTP}/${PROJECT}/${DATASET}/data/01.Data/
+        else
+            echo -e "[S3Uri error]: check provided path in .getdata.txt file."
+            exit 1
+        fi
     done
 fi
 
@@ -34,34 +50,23 @@ if [[ -f "${OTP}/${PROJECT}/${DATASET}/analysis/notebooks/.modules.txt" ]]; then
         
         if [[ -f ${OTP}/${PROJECT}/${DATASET}/analysis/notebooks/${module}/requirements.json ]]; then 
             # read file (json maybe better), copy data locally, edit path and change it into default parameters
-            echo  -e "Requirement file found! Checking required data.."
+            echo  -e "Requirement file found! Checking required data."
+            echo  -e "Data will be fetched from s3 bucket if not already present on local machine."
 
             for part in $(jq -r "keys[]" ${OTP}/${PROJECT}/${DATASET}/analysis/notebooks/${module}/requirements.json); do
-            
-                mkdir -p ${requirement_dir} # create a subdirectory with the outer key name in json
-                for key in $( jq -r --arg part ${part}  '.[$part] | keys[]' ${OTP}/${PROJECT}/${DATASET}/analysis/notebooks/${module}/requirements.json ); do
-                    file_path=$( jq -r --arg part ${part} --arg key ${key} '.[$part][$key] [0]' ${OTP}/${PROJECT}/${DATASET}/analysis/notebooks/${module}/requirements.json )
-                    file_name=$( basename ${file_path})
-                    file_type=$( jq -r --arg part ${part} --arg key ${key} '.[$part][$key] [1]' ${OTP}/${PROJECT}/${DATASET}/analysis/notebooks/${module}/requirements.json )
 
-                    if [[ ! -e ${requirement_dir}/${file_name} ]]; then # if file does not exist locally, copy it from s3 bucket
-                        echo -e "${file_name} not found in ${requirement_dir}. Fetching it from s3 bucket.."
-                        if [[ $(ast exist ${file_path}) == "True" ]]; then
-                            if [[ ${file_type} == "dir" ]]; then
-                                aws s3 cp ${file_path} ${requirement_dir}/${file_name} --recursive && echo -e "[DONE].\n"
-                            elif [[ ${file_type} == "file" ]]; then
-                                aws s3 cp ${file_path} ${requirement_dir}/${file_name} && echo -e "[DONE].\n"
-                            fi
-                        else 
-                            echo -e "[ERROR] file ${file_path} not found in s3 bucekt"
-                            exit 1
-                        fi
-                    else
-                        echo -e "${requirement_dir}/${file_name} found."
-                    fi
-                done
+                file_path=$( jq -r --arg part ${part}  '.[$part] ' ${OTP}/${PROJECT}/${DATASET}/analysis/notebooks/${module}/requirements.json )
+                file_name=$( basename ${file_path})
+                
+                if [[ $(ast exist ${file_path}) == "True" ]]; then
+                    echo -e "Checking: ${file_name}"
+                    ast syncup ${file_path} ${requirement_dir}/${file_name}
+                else 
+                    echo -e "[ERROR] file ${file_path} not found in s3 bucekt"
+                    exit 1
+                fi
             done
-            echo -e "[DONE]. All required files have been checked.\n"
+            echo -e "[DONE]. All required files have been checked and fetched.\n"
 
         else
             echo -e "No requirements.json file found. All data are assumed to be already available. \n[DONE].\n"
