f73ef0e78760a53ac1b28911c99b5afbbbf26548
luclabarriere <luc.labarriere@turing.bio>
2024-12-16 14:02:35 +0000

    Bucket style implementation + moved sorting algorithm to PropertyContainer (!340)

--- common/StringContainer.h
+++ common/StringContainer.h
@@ -9,9 +9,15 @@
 
 class StringContainer {
 public:
+    static constexpr size_t BUCKET_SIZE = 1024ul * 16;
     using ViewVector = std::vector<std::string_view>;
 
-    StringContainer() = default;
+    StringContainer()
+        : _buckets(1)
+    {
+        _buckets.back().resize(BUCKET_SIZE);
+    }
+
     ~StringContainer() = default;
     StringContainer(StringContainer&& other) = default;
     StringContainer& operator=(StringContainer&& other) = default;
@@ -19,16 +25,17 @@ public:
     StringContainer& operator=(const StringContainer& other) = delete;
 
     void alloc(std::string_view content) {
-        msgbioassert(!_built, "StringContainer was already built");
+        if (_remainingSize < content.size()) {
+            _remainingSize = BUCKET_SIZE;
+            auto& chars = _buckets.emplace_back();
+            chars.resize(BUCKET_SIZE);
+        }
 
-        const size_t offset = _chars.size();
+        auto& chars = _buckets.back();
+        const size_t offset = chars.size() - _remainingSize;
 
-        auto& repr = _reprs.emplace_back();
-        repr._offset = offset;
-        repr._size = content.size();
-
-        _chars.resize(_chars.size() + content.size());
-        std::memcpy(_chars.data() + offset, content.data(), content.size());
+        std::memcpy(chars.data() + offset, content.data(), content.size());
+        _views.push_back({chars.data() + offset, content.size()});
     }
 
 
@@ -39,47 +46,16 @@ public:
 
     size_t size() const { return _views.size(); }
 
-    void build() {
-        msgbioassert(!_built, "StringContainer was already built");
-        _views.resize(_reprs.size());
-
-        for (size_t i = 0; i < _views.size(); i++) {
-            const auto& repr = _reprs[i];
-            _views[i] = {_chars.data() + repr._offset, repr._size};
-        }
-
-        _built = true;
-        _reprs.clear();
-    }
-
-    void sort(const std::span<const size_t>& newOrder) {
-        std::vector<char> newChars;
-        ViewVector newViews;
-
-        newChars.resize(_chars.size());
-        newViews.resize(_views.size());
-
-        size_t offset = 0;
-        size_t i = 0;
-
-        for (const size_t stringIndex : newOrder) {
-            const std::string_view& v = _views[stringIndex];
-
-            // Memcpy including null terminator
-            char* dst = newChars.data() + offset;
-            std::memcpy(dst, v.data(), v.size());
-            newViews[i] = {dst, v.size()};
-            offset += v.size();
-
-            i++;
-        }
-
-
-        _views = std::move(newViews);
-        _chars = std::move(newChars);
-    }
-
     const ViewVector& get() const { return _views; }
+
+    size_t byteCount() const {
+        size_t count = 0;
+        for (const auto& bucket : _buckets) {
+            count += bucket.size();
+        }
+        return count;
+    }
+
     ViewVector::const_iterator begin() const { return _views.begin(); }
     ViewVector::const_iterator end() const { return _views.end(); }
 
@@ -87,13 +63,11 @@ private:
     struct StringRepr {
         size_t _offset {};
         size_t _size {};
+        size_t _bucket {};
     };
 
-    std::vector<char> _chars;
+    std::vector<std::vector<char>> _buckets;
+    size_t _remainingSize = BUCKET_SIZE;
     ViewVector _views;
-
-    // Only used before build()
-    bool _built = false;
-    std::vector<StringRepr> _reprs;
 };
 
