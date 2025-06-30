c3e36a8aec1bb21b7c1705a42b54b52cd620c50d
cyrusknopf <cyrus.knopf@turing.bio>
2025-06-30 16:27:54 +0000

    Find implemented (!67)

--- samples/prefix-tree/prefix-tree.cpp
+++ samples/prefix-tree/prefix-tree.cpp
@@ -1,6 +1,7 @@
 #include <cctype>
 #include <iostream>
 #include <array>
+#include <string_view>
 #include <vector>
 
 #include "utils.h"
@@ -17,6 +18,14 @@ constexpr size_t SIGMA = 26 + 10;
 
 class StringApproximatorIndex {
 public:
+    enum FindResult {
+        FOUND,
+        FOUND_PREFIX,
+        NOT_FOUND
+    };
+
+
+
     struct PrefixTreeNode {
         array<PrefixTreeNode*, SIGMA> _children{};
         // Termination char denotes terminal node
@@ -38,6 +47,12 @@ public:
 
     };
 
+    struct PrefixTreeIterator {
+        FindResult _result{NOT_FOUND};
+        PrefixTreeNode* _nodePtr{nullptr};
+    };
+
+
 
     StringApproximatorIndex()
     {
@@ -45,6 +60,8 @@ public:
     }
 
     void insert(string_view str) { return _insert(_root, str); }
+
+    PrefixTreeIterator find(string_view str) { return _find(_root, str); }
 
     void print() const { _printTree(_root); }
 
@@ -64,9 +81,9 @@ private:
         else return 26 + c - '0';
     }
 
-    void _insert(PrefixTreeNode* n, string_view str) {
-        PrefixTreeNode* node = n;
-        for (const char c : str) {
+    void _insert(PrefixTreeNode* root, string_view sv) {
+        PrefixTreeNode* node = root;
+        for (const char c : sv) {
             size_t idx = charToIndex(c);
             if (node->_children[idx] == nullptr) {
                 auto* newChild = new PrefixTreeNode(c);
@@ -75,6 +92,28 @@ private:
             node = node->_children[idx];
         }
         node->_isComplete = true;
+    }
+
+
+    PrefixTreeIterator _find(PrefixTreeNode* root, string_view sv) {
+        char firstChar = sv[0];
+        size_t idx = charToIndex(firstChar);
+        if (root->_children[idx] == nullptr) {
+            return PrefixTreeIterator{NOT_FOUND, nullptr};
+        }
+
+        sv.remove_prefix(1);        
+
+        PrefixTreeNode* node = root->_children[idx];
+        for (const char c : sv) {
+            size_t idx = charToIndex(c);
+            if (node->_children[idx] == nullptr) {
+                return PrefixTreeIterator{NOT_FOUND, node};
+            }
+            node = node->_children[idx];
+        }
+        FindResult res = node->_isComplete ? FOUND : FOUND_PREFIX;
+        return PrefixTreeIterator{res, node};
     }
 
     void _insert_rec(PrefixTreeNode* parent, string_view str) {
@@ -98,19 +137,6 @@ private:
         }
     }
 
-    enum FindResult {
-        FOUND,
-        FOUND_PREFIX,
-        NOT_FOUND
-    };
-
-    FindResult _find(PrefixTreeNode* parent, string_view str) {
-        if (str.empty()) {
-            parent->_isComplete = true;
-            return FOUND;
-        }
-        
-    }
 
    void _printTree(PrefixTreeNode* node) const {
         _printTree(node, "", true);
@@ -165,6 +191,24 @@ int main() {
             else tree.insert(tokens[1]);
         }
         else if (tokens[0] == "p") tree.print();
+        else if (tokens[0] == "f") {
+            if (tokens.size() != 2) cout << "f <param>" << endl;
+            else {
+                StringApproximatorIndex::PrefixTreeIterator res = tree.find(tokens[1]);
+                string verb;
+                if (res._result == StringApproximatorIndex::FOUND) {
+                    verb = " found ";
+                }
+                else if (res._result == StringApproximatorIndex::FOUND_PREFIX) {
+                    verb = " found as a subtring ";
+                }
+                else {
+                    verb = " not found ";
+                }
+                cout << "String " << tokens[1] << verb << endl;
+            }
+            
+        }
         else cout << "unkown cmd" << endl;
     }
 
